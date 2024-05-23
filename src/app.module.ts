655d9babb33adbccfd197ba35c98deb6c726ce07
bek <shoyatbekov03032003@gmail.com>
2024-05-23 16:59:29 +0500

    1.0.3 made public public

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,16 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { AvatarsModule } from './avatars/avatars.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [AuthModule, UsersModule, AvatarsModule],
+  imports: [
+    AuthModule,
+    UsersModule,
+    AvatarsModule,
+    ServeStaticModule.forRoot({ rootPath: join(__dirname, '..', 'public') }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

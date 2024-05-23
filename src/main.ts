48d3657021bf7d786c017c889e9e4a15fe8fdfa1
bek <shoyatbekov03032003@gmail.com>
2024-05-23 16:33:27 +0500

    1.0.1

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import * as morgan from 'morgan';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.use(morgan('dev'));
+  app.use(morgan('common'));
 
   await app.listen(3000);
 }

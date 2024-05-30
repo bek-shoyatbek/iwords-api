ee31030ba9fc4ac2809fbedb523c7acdb8124ee7
bek <shoyatbekov03032003@gmail.com>
2024-05-30 10:32:45 +0500

    morgan logs status changed to 'dev'

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import * as morgan from 'morgan';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.use(morgan('common'));
+  app.use(morgan('dev'));
 
   await app.listen(3000);
 }
